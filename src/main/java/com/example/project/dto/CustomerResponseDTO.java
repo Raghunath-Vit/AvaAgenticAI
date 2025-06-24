3d5d3e5f1e1ee20740b335f840700e93b53e255f
Raghunath-Vit <raghunathsinghmannu1@gmail.com>
2025-06-24 17:28:31 +0530

    Add CustomerResponseDTO for API responses

---
+++ src/main/java/com/example/project/dto/CustomerResponseDTO.java
@@ -0,0 +1,11 @@
+package com.example.project.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+}
